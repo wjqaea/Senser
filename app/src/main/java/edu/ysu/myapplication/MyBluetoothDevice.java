07b173c946c4cff498ad26ac5424f09368c9bdfa
Dylan Cat <wjqaea@163.com>
2021-08-03 19:11:50 +0800

    8.3 添加蓝牙rssi记录 未完成
    添加按钮以记录此时周围蓝牙设备rssi信息功能。

---
+++ app/src/main/java/edu/ysu/myapplication/MyBluetoothDevice.java
@@ -0,0 +1,33 @@
+package edu.ysu.myapplication;
+
+import android.bluetooth.BluetoothDevice;
+
+/**
+ * Created by G on 2019/8/14.
+ */
+
+public class MyBluetoothDevice {
+
+    private BluetoothDevice bluetoothDevice;
+    private int Rssi;
+
+    public MyBluetoothDevice(BluetoothDevice bluetoothDevice, int rssi) {
+        this.bluetoothDevice = bluetoothDevice;
+        Rssi = rssi;
+    }
+
+    public BluetoothDevice getBluetoothDevice() {
+        return bluetoothDevice;
+    }
+
+    public void setBluetoothDevice(BluetoothDevice bluetoothDevice) {
+        this.bluetoothDevice = bluetoothDevice;
+    }
+
+    public int getRssi() {
+        return Rssi;
+    }
+    public void setRssi(int rssi) {
+        Rssi = rssi;
+    }
+}
--- app/src/main/java/edu/ysu/myapplication/service/RssiService.java
+++ app/src/main/java/edu/ysu/myapplication/service/RssiService.java
@@ -1,16 +1,88 @@
 package edu.ysu.myapplication.service;
 
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.ysu.myapplication.MyBluetoothDevice;
+
 public class RssiService extends Service {
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private Map<String, MyBluetoothDevice> myBluetoothDeviceMap;
+    private boolean mScanning;
+    private Handler mHandler;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final long SCAN_PERIOD = 10000;
+
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mHandler = new Handler();
+        myBluetoothDeviceMap = new HashMap<>();
+
+
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "不支持蓝牙", Toast.LENGTH_SHORT).show();
+            stopSelf();
+        }
+    }
+
+
+
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+                @Override
+                public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+
+                }
+            };
+
+
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
     }
 }
