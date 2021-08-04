427dafc4b51a5252d6d94a62828a7704bdb6e070
Dylan Cat <wjqaea@163.com>
2021-08-04 18:21:10 +0800

    增加Service获取权限相关功能

--- app/src/main/java/edu/ysu/myapplication/MainActivity.java
+++ app/src/main/java/edu/ysu/myapplication/MainActivity.java
@@ -1,12 +1,26 @@
 package edu.ysu.myapplication;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.ysu.myapplication.service.AccelerometerService;
 import edu.ysu.myapplication.service.GyroscopeService;
@@ -22,8 +36,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Intent intentMagneticService;
     Intent intentRssiService;
 
+
+    private static PermissionListener mlistener;
+
     int btFlag = 0;
-    long baseTime = 1624408500;
+    long baseTime = 1628042000;
+
+
+    ServiceConnection conn = new ServiceConnection() {
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            // 当service绑定成功时，会调用次方法，可以在此申请权限
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},1);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +73,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonStart.setOnClickListener(this);
         buttonStop.setOnClickListener(this);
         buttonRssi.setOnClickListener(this);
-        intentAccelerometerService = new Intent(MainActivity.this , AccelerometerService.class);
-        intentGyroscopeService = new Intent(MainActivity.this , GyroscopeService.class);
-        intentMagneticService = new Intent(MainActivity.this , MagneticService.class);
-        intentRssiService = new Intent(MainActivity.this , RssiService.class);
+//        intentAccelerometerService = new Intent(MainActivity.this , AccelerometerService.class);
+//        intentGyroscopeService = new Intent(MainActivity.this , GyroscopeService.class);
+//        intentMagneticService = new Intent(MainActivity.this , MagneticService.class);
+//        intentRssiService = new Intent(MainActivity.this , RssiService.class);
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -82,7 +116,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String dateString = String.valueOf(System.currentTimeMillis()/1000 - baseTime);
         intentRssiService = new Intent(MainActivity.this , RssiService.class);
         intentRssiService.putExtra("dateString" , dateString);
-        startService(intentRssiService);
+        bindService(intentRssiService,conn, Context.BIND_AUTO_CREATE);
+        //startService(intentRssiService);
     }
 
     private void stopServices(){
@@ -95,4 +130,86 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         btFlag = 0;
     }
+
+
+
+
+    /**
+     * 权限申请
+     * @param permissions 待申请的权限集合
+     * @param listener  申请结果监听事件
+     */
+    public static void requestRunTimePermission(String[] permissions, PermissionListener listener) {
+        mlistener = listener;
+
+        Activity topActivity = ActivityCollector.getTopActivity();
+        if (topActivity == null) {
+            return;
+        }
+
+        //用于存放为授权的权限
+        List<String> permissionList = new ArrayList<>();
+        //遍历传递过来的权限集合
+        for (String permission : permissions) {
+            //判断是否已经授权
+            if (ContextCompat.checkSelfPermission(topActivity, permission) != PackageManager.PERMISSION_GRANTED) {
+                //未授权，则加入待授权的权限集合中
+                permissionList.add(permission);
+            }
+        }
+
+        //判断集合
+        if (!permissionList.isEmpty()) {  //如果集合不为空，则需要去授权
+            ActivityCompat.requestPermissions(topActivity, permissionList.toArray(new String[permissionList.size()]), 1);
+        } else {  //为空，则已经全部授权
+            listener.onGranted();
+        }
+    }
+
+
+    /**
+     * 权限申请结果
+     * @param requestCode  请求码
+     * @param permissions  所有的权限集合
+     * @param grantResults 授权结果集合
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0){
+                    //被用户拒绝的权限集合
+                    List<String> deniedPermissions = new ArrayList<>();
+                    //用户通过的权限集合
+                    List<String> grantedPermissions = new ArrayList<>();
+                    for (int i = 0; i < grantResults.length; i++) {
+                        //获取授权结果，这是一个int类型的值
+                        int grantResult = grantResults[i];
+
+                        if (grantResult != PackageManager.PERMISSION_GRANTED){ //用户拒绝授权的权限
+                            String permission = permissions[i];
+                            deniedPermissions.add(permission);
+                        }else{  //用户同意的权限
+                            String permission = permissions[i];
+                            grantedPermissions.add(permission);
+                        }
+                    }
+
+                    if (deniedPermissions.isEmpty()){  //用户拒绝权限为空
+                        mlistener.onGranted();
+                    }else {  //不为空
+                        //回调授权成功的接口
+                        mlistener.onDenied(deniedPermissions);
+                        //回调授权失败的接口
+                        mlistener.onGranted(grantedPermissions);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+
 }
--- app/src/main/java/edu/ysu/myapplication/service/RssiService.java
+++ app/src/main/java/edu/ysu/myapplication/service/RssiService.java
@@ -1,13 +1,15 @@
 package edu.ysu.myapplication.service;
 
+import android.Manifest;
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -16,30 +18,55 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-import edu.ysu.myapplication.MyBluetoothDevice;
+import edu.ysu.myapplication.MainActivity;
+import edu.ysu.myapplication.PermissionListener;
+
 
 public class RssiService extends Service {
 
     private BluetoothAdapter mBluetoothAdapter;
-    private Handler mHandler;
-    private static final int REQUEST_ENABLE_BT = 1;
+//    private Handler mHandler = new Handler();
     private static final long SCAN_PERIOD = 10000;
     private BufferedWriter bufferedWriter;
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        Log.d("TAG", "onBind: in");
+
+        requestPermission();
+
+        String dateString = intent.getStringExtra("dateString");
+        String pathString = getExternalFilesDir(null).getAbsolutePath()+"/"+dateString+"/";
+        File path = new File(pathString);
+        boolean mkd = false;
+        if (!path.exists() || !path.isDirectory()) mkd = path.mkdirs();
+        if (!mkd) Toast.makeText(this, "文件夹创建失败", Toast.LENGTH_SHORT).show();
+        File file = new File(pathString,"RSSIData.csv");
+        try {
+            if(!file.exists()){
+                boolean isCreate =  file.createNewFile();
+                if (isCreate) {
+                    Toast.makeText(this, "RSSI数据创建成功", Toast.LENGTH_SHORT).show();
+                }
+            }
+            bufferedWriter = new BufferedWriter(new FileWriter(file , true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        scanLeDevice(true);
         return null;
     }
+
+
 
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mHandler = new Handler();
 
         final BluetoothManager bluetoothManager =
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
@@ -50,6 +77,8 @@ public class RssiService extends Service {
             stopSelf();
         }
     }
+
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
@@ -77,44 +106,6 @@ public class RssiService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
-
-
-
-    private BluetoothAdapter.LeScanCallback mLeScanCallback =
-            new BluetoothAdapter.LeScanCallback() {
-                @Override
-                public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
-                    try {
-                        bufferedWriter.write(""+device.getAddress());
-                        bufferedWriter.write(",");
-                        bufferedWriter.write(""+device.getName());
-                        bufferedWriter.write(",");
-                        bufferedWriter.write(""+rssi);
-                        bufferedWriter.newLine();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-
-
-
-    private void scanLeDevice(final boolean enable) {
-        if (enable) {
-            // Stops scanning after a pre-defined scan period.
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
-                }
-            }, SCAN_PERIOD);
-            mBluetoothAdapter.startLeScan(mLeScanCallback);
-        } else {
-            mBluetoothAdapter.stopLeScan(mLeScanCallback);
-        }
-    }
-
-
     @Override
     public void onDestroy() {
         try {
@@ -124,5 +115,80 @@ public class RssiService extends Service {
             e.printStackTrace();
         }
         super.onDestroy();
+    }
+
+//    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+//            new BluetoothAdapter.LeScanCallback() {
+//                @Override
+//                public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+//                    try {
+//                        bufferedWriter.write(""+device.getAddress());
+//                        bufferedWriter.write(",");
+//                        bufferedWriter.write(""+device.getName());
+//                        bufferedWriter.write(",");
+//                        bufferedWriter.write(""+rssi);
+//                        bufferedWriter.newLine();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            };
+
+    private final ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            try {
+                bufferedWriter.write(""+result.getDevice().getAddress());
+                bufferedWriter.write(",");
+                bufferedWriter.write(""+result.getDevice().getName());
+                bufferedWriter.write(",");
+                bufferedWriter.write(""+result.getRssi());
+                bufferedWriter.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            super.onScanResult(callbackType, result);
+        }
+    };
+
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+//            mHandler.postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    mBluetoothAdapter.getBluetoothLeScanner().stopScan(scanCallback);
+//                    // mBluetoothAdapter.stopLeScan(mLeScanCallback);
+//                }
+//            }, SCAN_PERIOD);
+            //mBluetoothAdapter.startLeScan(mLeScanCallback);
+            mBluetoothAdapter.getBluetoothLeScanner().startScan(scanCallback);
+        } else {
+            //mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            mBluetoothAdapter.getBluetoothLeScanner().stopScan(scanCallback);
+        }
+    }
+
+
+    private void requestPermission() {
+        MainActivity.requestRunTimePermission(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}
+                , new PermissionListener() {
+                    @Override
+                    public void onGranted() {
+
+                    }
+
+                    @Override
+                    public void onGranted(List<String> grantedPermission) {
+
+                    }
+
+                    @Override
+                    public void onDenied(List<String> deniedPermission) {
+
+                    }
+                });
+
     }
 }
